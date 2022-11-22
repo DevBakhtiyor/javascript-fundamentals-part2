0e263a85b2abb7d7fe127fb04899d63b39daede6
DevBakhtiyor <bakhtiyor.dev.it@gmail.com>
2022-11-21 21:17:19 -0500

    Start JavaScript Fundamentals Part 2

---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+/////////////////////////////
+// Activating Strict Mode //
+/////////////////////////////
+
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534;
+// const if = 23;
+
+/////////////////////////////
+// Functions //
+/////////////////////////////
+
+function logger() {
+  console.log("My name is Bakhtiyor");
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+// Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
