def8a891df9b248e467f8ab0ec500c9396311b59
DevBakhtiyor <bakhtiyor.dev.it@gmail.com>
2022-12-06 20:17:50 -0500

    Learing JavaScript Fundamentals

--- script.js
+++ script.js
@@ -133,7 +133,6 @@ scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
-*/
 
 // Introduction to Arrays
 
@@ -176,3 +175,244 @@ const ages = [
   calcAge(years[years.length - 1]),
 ];
 console.log(ages);
+
+// Basic Array Operation (Methods)
+
+const friends = ["Michael", "Steven", "Peter"];
+// Add elements
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
+
+// Introduction to Objects
+
+const jonArray = [
+  'Jon',
+  'Toshmatov',
+  2037 - 1981,
+  'teacher',
+  ['Michael', 'Peter', 'Steven']
+];
+
+const jon = {
+  firstName: "Jon",
+  lastName: "Toshmatov",
+  age: 2037 - 1981,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jon);
+
+console.log(jon.lastName);
+console.log(jon["lastName"]);
+
+const nameKey = "Name";
+console.log(jon["first" + nameKey]);
+console.log(jon["last" + nameKey]);
+
+//console.log(jon."last" + nameKey); its not working
+
+const interestedIn = prompt(
+  "What do you want to know about Jon? Choose between firstName, lastName, age, job and friends"
+);
+if (jon[interestedIn]) {
+  console.log(jon[interestedIn]);
+} else {
+  console.log(
+    "Wrong request! Choose between firstName, lastName, age, job and friends"
+  );
+}
+
+jon.location = "Portugal";
+jon["twitter"] = "@jontoshmatov";
+console.log(jon);
+console.log(
+  `${jon.firstName} has ${jon.friends.length} friends, and his best friend is called ${jon.friends[0]}`
+);
+
+const jon = {
+  firstName: "Jon",
+  lastName: "Toshmatov",
+  birthYear: 1981,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+
+  // calcAge: function () {
+  //   console.log(this);
+  //   return 2037 - this.birthYear;
+  // },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      this.job
+    }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+  },
+};
+console.log(jon.calcAge());
+// console.log(jon["calcAge"](1981));
+console.log(jon.age);
+console.log(jon.getSummary());
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`
+  );
+}
+
+// console.log("Lifting weights repetition 1 🏋️‍♀️");
+// console.log("Lifting weights repetition 2 🏋️‍♀️");
+// console.log("Lifting weights repetition 3 🏋️‍♀️");
+// console.log("Lifting weights repetition 4 🏋️‍♀️");
+// console.log("Lifting weights repetition 5 🏋️‍♀️");
+// console.log("Lifting weights repetition 6 🏋️‍♀️");
+// console.log("Lifting weights repetition 7 🏋️‍♀️");
+// console.log("Lifting weights repetition 8 🏋️‍♀️");
+// console.log("Lifting weights repetition 9 🏋️‍♀️");
+// console.log("Lifting weights repetition 10 🏋️‍♀️");
+
+// for Loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
+
+const jon = [
+  "Jon",
+  "Toshmatov",
+  2037 - 1981,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+
+// console.log(jon[0])
+// console.log(jon[1])
+// ......
+// console.log(jon[4])
+
+for (let he = 0; he < jon.length; he++) {
+  // Reading from Jon array
+  console.log(jon[he], typeof jon[he]);
+
+  // Filling types array
+  // types[he] = typeof jon[he];
+  types.push(typeof jon[he]);
+}
+console.log(types);
+
+const years = [1981, 1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let he = 0; he < years.length; he++) {
+  ages.push(2037 - years[he]);
+}
+console.log(ages);
+
+// continue and break
+console.log("--- ONLY STRINGS ---");
+for (let he = 0; he < jon.length; he++) {
+  if (typeof jon[he] !== "string") continue;
+
+  console.log(jon[he], typeof jon[he]);
+}
+
+console.log("--- BREAK WITH NUMBER ---");
+for (let he = 0; he < jon.length; he++) {
+  if (typeof jon[he] === "number") break;
+
+  console.log(jon[he], typeof jon[he]);
+}
+*/
+
+const jon = [
+  "Jon",
+  "Toshmatov",
+  2037 - 1981,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let he = jon.length - 1; he >= 0; he--) {
+  console.log(he, jon[he]);
+}
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------ Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise} Lifting weight repetition ${rep} 🧛`);
+  }
+}
